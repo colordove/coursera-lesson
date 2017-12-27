0ff766bee5f7b9858c35feac6238e159908d3db9
HuJian <624598206@qq.com>
2017-12-27 12:21:54 +0800

    Initial Commit

---
+++ coursera/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coursera
+//
+//  Created by Jian Hu on 27/12/2017.
+//  Copyright © 2017 Jian Hu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
