f2cb626ac957a51edde458f064bceebc542ee963
HuJian <624598206@qq.com>
2018-03-08 10:19:25 +0800

    update

---
+++ coursera/hex.cpp
@@ -0,0 +1,16 @@
+//
+//  hex.cpp
+//  coursera
+//
+//  Created by Jian Hu on 11/01/2018.
+//  Copyright © 2018 Jian Hu. All rights reserved.
+//
+
+#include <stdio.h>
+using namespace std;
+#include <iostream>
+int main() {
+    int a = -123;
+    cout << hex << a << endl;
+    return 0;
+}
---
+++ coursera/sizeof.cpp
@@ -0,0 +1,17 @@
+//
+//  sizeof.cpp
+//  coursera
+//
+//  Created by Jian Hu on 11/01/2018.
+//  Copyright © 2018 Jian Hu. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "sizeof(short int)=" << sizeof(short int) << endl;
+    cout << "sizeof(int)=" << sizeof(int) << endl;
+    cout << "sizeof(long int)=" << sizeof(long int) << endl;
+    return 0;
+}
